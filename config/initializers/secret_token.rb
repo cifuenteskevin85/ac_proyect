1dc7dacffab57403e2a8ee9af01555380b316945
kevin cifuentes <cifuenteskevin85@gmail.com>
2012-11-15 11:18:42 -0500

    Cra ar una aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcProyect::Application.config.secret_token = 'c91bd5ca915520622cb677f8f76cf863623e53addf93f046d949b3c229f2687a56e0047a8912e6c0d8341e286cb48e8b6a00ee2962b7438ac558c44a3e13f107'
